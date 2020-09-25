c286eb617e1fa15ea1194abfd0df5af4c72bc764
jswpark <jswpark@gmail.com>
2020-09-25 17:06:43 +0700

    nter the commit message for your changes. Lines starting

--- webcrawlerTiki.py
+++ webcrawlerTiki.py
@@ -1,4 +1,44 @@
-# import the website
+# selecting Tiki products
+# Laptops - IT Equipment
+# https://tiki.vn/laptop-may-vi-tinh-linh-kien/c1846?src=c.1846.hamburger_menu_fly_out_banner
+# https://tiki.vn/laptop-may-vi-tinh-linh-kien/c1846?src=c.1846.hamburger_menu_fly_out_banner&page=2
+
+# function to import the website
+# need to crawl through x pages
+# add sleep function
+''' example code from class
+import requests
+
+r = requests.get('https://vnexpress.net/')
+
+print(r.text)
+''''
+
+# function to prase the raw text with beautiful soup
+''' example code from class
+from bs4 import BeautifulSoup
+
+# r.text is a HTML file so we will use html.parser
+soup = BeautifulSoup(r.text, 'html.parser')
+
+'''
+
+# function to extract information
+''' need the following
+- product ID
+- product price
+- product image
+'''
+
+# function to convert to pandas dataframe
+'''
+import pandas as pd
+
+articles = pd.DataFrame(data = data, columns = data[0].keys())
+'''
 
 
-
+# function to store result
+'''
+articles.to_csv("./result.csv", index=False)
+'''
