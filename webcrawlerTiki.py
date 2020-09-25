e8216e8e6de18b17ece81e250a8c5908cd2f15f4
jswpark <jswpark@gmail.com>
2020-09-25 20:53:39 +0700

    another update

--- webcrawlerTiki.py
+++ webcrawlerTiki.py
@@ -3,25 +3,31 @@
 # https://tiki.vn/laptop-may-vi-tinh-linh-kien/c1846?src=c.1846.hamburger_menu_fly_out_banner
 # https://tiki.vn/laptop-may-vi-tinh-linh-kien/c1846?src=c.1846.hamburger_menu_fly_out_banner&page=2
 
-# function to import the website
-# need to crawl through x pages
-# add sleep function
-''' example code from class
+# function to import the website and prase the raw text with beautiful soup
+# Todo1: need to add for loop to crawl through x pages
+# Todo2: add sleep function so Tiki does not kick us out
+''' 
 import requests
-
-r = requests.get('https://vnexpress.net/')
-
-print(r.text)
-'''
-
-# function to prase the raw text with beautiful soup
-''' example code from class
 from bs4 import BeautifulSoup
 
-# r.text is a HTML file so we will use html.parser
-soup = BeautifulSoup(r.text, 'html.parser')
+url = 'https://tiki.vn/laptop-may-vi-tinh-linh-kien/c1846?src=c.1846.hamburger_menu_fly_out_banner'
 
+def get_url(url):
+    """Get parsed HTML from url
+      Input: url to the webpage
+      Output: Parsed HTML text of the webpage
+    """
+    # Send GET request
+    r = requests.get(url)
+
+    # Parse HTML text
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    return soup
 '''
+# saving data set for testing
+# soup = get_url(url) # this brings the soup variable to a global status
+# soup_fortesting = soup
 
 # function to extract information
 ''' need the following
